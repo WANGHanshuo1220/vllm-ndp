03a7226ce02e7b1408eb73de6b77bfc90d56e293
wanghanshuo <wanghanshuo1220@gmail.com>
2025-01-20 13:33:45 +0800

    add run-server.sh in rdma

--- vllm/mem_pool/rdma/run-server.sh
+++ vllm/mem_pool/rdma/run-server.sh
@@ -2,6 +2,6 @@ python vllm/mem_pool/rdma/server_rdma.py \
     --max-model-len 128 \
     --max-num-seqs 8 \
     --swap-space 1 \
-    --tensor-parallel 4 \
-    --pipeline-parallel 2 \
+    --tensor-parallel 1 \
+    --pipeline-parallel 1 \
     --model /root/model/meta-llama/Llama-3.1-8B-Instruct/ 
